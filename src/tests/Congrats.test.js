c3b9e32e656daf969c2ec2730750c1dde753d24b
Pedro Silva <pedro.rls93@gmail.com>
2020-05-10 17:33:57 -0300

    improvement(tests): add enzyme adapter configuration to setupTests

--- src/tests/Congrats.test.js
+++ src/tests/Congrats.test.js
@@ -1,10 +1,7 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
-import EnzymeAdapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
 import { Congrats } from "../components/Congrats";
 import { findByTestAttr, checkProps } from "./testsUtils";
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 const setup = (props = {}) => shallow(<Congrats {...props} />);
 
