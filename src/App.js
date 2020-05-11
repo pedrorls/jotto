02d6564c6188387e922abb764a5fb7ed189e9493
Pedro Silva <pedro.rls93@gmail.com>
2020-05-11 09:51:04 -0300

    improvement(app): connect with redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { Congrats } from "./components/Congrats";
 import { GuessedWords } from "./components/GuessedWords";
+import Input from "./components/Input";
 import "./App.css";
+import { connect } from "react-redux";
 
-const App = () => {
-  return (
-    <div data-test="main-app" className="container">
-      <h1>Jotto App</h1>
-      <Congrats success />
-      <GuessedWords />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const { success } = this.props;
+    return (
+      <div data-test="main-app" className="container">
+        <h1>Jotto App</h1>
+        {success ? <Congrats success /> : <Input />}
+        <GuessedWords />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { success: state.successReducer };
 };
 
-export default App;
+export default connect(mapStateToProps)(App);
